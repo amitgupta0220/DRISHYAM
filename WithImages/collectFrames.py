fab006817a21e2bf08b04c14d5de5bb23dc5a632
amitgupta0220 <amitguptaag07@gmail.com>
2021-12-25 00:11:58 +0530

    new approach

--- WithImages/collectFrames.py
+++ WithImages/collectFrames.py
@@ -8,8 +8,12 @@ labels = ["hello"]
 numberOfImages = 15
 
 for label in labels:
+    count = 0
     path = os.path.join(os.getcwd(), IMAGE_PATH, label)
-    os.mkdir(path)
+    try:
+        os.mkdir(path)
+    except:
+        pass
     cap = cv2.VideoCapture(0)
     print("Capturing images for {}".format(label))
     time.sleep(5)
@@ -17,8 +21,11 @@ for label in labels:
         ret, frame = cap.read()
         imageName = os.path.join(
             os.getcwd(), IMAGE_PATH, label, label+'.'+'{}.jpg'.format(count))
-        cv2.imwrite(imageName, frame)
-        cv2.imshow("Capturing", frame)
+        img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0)
+        edges = cv2.Canny(image=img_blur, threshold1=80, threshold2=180)
+        cv2.imwrite(imageName, edges)
+        cv2.imshow("Capturing", edges)
         time.sleep(2)
         count = count+1
 
